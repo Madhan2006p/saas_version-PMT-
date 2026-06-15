256dd2f56390962c939ee45c1fc0ac003476a800
Madhan2006p <madhanp28072006@gmail.com>
2026-06-15 19:29:51 +0530

    Fix migration error by squashing migrations on build

--- nexus_backend/build.sh
+++ nexus_backend/build.sh
@@ -3,5 +3,14 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# Delete all old migrations to prevent SQLite recreation errors
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+
+# Create fresh migrations
+python manage.py makemigrations
+
+# Migrate and seed
 python manage.py migrate
 python manage.py seed_demo_data --skip-keycloak --skip-permissions
